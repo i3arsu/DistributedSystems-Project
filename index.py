7b0dda4a1399fe9594639b728fa3d8d96944bc15
Luka Sever <luka.sever03@gmail.com>
2023-12-29 11:05:46 +0100

    Implemented router with crud functionality, Created a sqlalchemy db with user model and schema

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from routes.crypt import router as crypt_router
+from routes.crypt import router as main_router
 
 app = FastAPI()
 
-app.include_router(crypt_router)
+app.include_router(main_router)
